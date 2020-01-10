76fb67f5cd05ca6868db7489ea6ea348c708f641
Cristian Cominelli <firegarden1977@gmail.com>
2020-01-10 18:42:47 +0100

    first commit

---
+++ src/app/App.java
@@ -0,0 +1,46 @@
+package app;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpSession;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class App extends HttpServlet
+{
+     //Instance variable used for counting hits on this servlet
+     private static final long serialVersionUID = -3967314453512919811L;
+     private int iHitCounter;
+
+     //init method just initializes the hitCounter to zero
+     public void init() throws ServletException
+     {
+          
+     }
+     public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
+     {
+          HttpSession session = request.getSession();
+          response.setContentType("text/html");
+          PrintWriter out =  response.getWriter();
+          
+          if(session.getAttribute("Counter")==null){
+               session.setAttribute("Counter", 1);
+               out.println("<h3>Benvenuto !</h3>");
+               out.println("E' la prima volta che visiti questo sito");
+          }
+          else{
+               session.setAttribute("Counter",((int)session.getAttribute("Counter"))+1);
+               out.println("<form>");
+               out.println("<h3>Benvenuto !</h3>");
+               out.println("Hai visitato questo sito: "+ (++iHitCounter));
+               out.println("</form>");
+          }
+          
+     }
+     public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
+     {
+          doGet(request, response);
+     }
+}
