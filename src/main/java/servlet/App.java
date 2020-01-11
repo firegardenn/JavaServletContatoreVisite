d7feb8874f5a33190ddfc845992a398ae0960e96
Cristian Cominelli <firegarden1977@gmail.com>
2020-01-11 14:29:28 +0100

    corrected app.java

--- src/main/java/servlet/App.java
+++ src/main/java/servlet/App.java
@@ -38,7 +38,7 @@ public class App extends HttpServlet
                session.setAttribute("Counter",(((int)session.getAttribute("Counter"))+1));
                out.println("<form>");
                out.println("<h3>Benvenuto !</h3>");
-               out.println("Hai visitato questo sito: "+ session.getAttribute("Counter"));
+               out.println("Hai visitato questo sito "+ session.getAttribute("Counter") + " volte.");
                out.println("</form>");
           }
           
